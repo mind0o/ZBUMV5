d2c13242803eac24c784336e5457941523198cfe
mind0o <75039290+mind0o@users.noreply.github.com>
2021-12-24 20:16:35 +0400

    Update mainCommunicatorV2.js

--- mainCommunicatorV2.js
+++ mainCommunicatorV2.js
@@ -1,1 +1,8 @@
-
+const WebSocket = require('ws')
+const wss = new WebSocket.WebSocketServer({ port: 8080, path: "/ZBUMCommunicate" });
+var express = require('express');
+const cors = require('cors');
+const app = express();
+const { json, raw } = require('express');
+const url = require('url');
+const { stringify } = require('querystring');
