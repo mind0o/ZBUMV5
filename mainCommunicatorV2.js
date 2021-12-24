ce40ad351ba2c9dde47c30b940bdf13c8ab70c18
mind0o <75039290+mind0o@users.noreply.github.com>
2021-12-24 20:23:03 +0400

    Update mainCommunicatorV2.js

--- mainCommunicatorV2.js
+++ mainCommunicatorV2.js
@@ -1,12 +1,12 @@
 var express = require('express');
 const cors = require('cors');
 const app = express();
-
-app.use(cors())
-app.use(express.json());
 const { json, raw } = require('express');
 const url = require('url');
 const { stringify } = require('querystring');
+app.use(cors())
+app.use(express.json());
+
 
 var takenGameIDs = [];
 var gameInfosJSON = [];
