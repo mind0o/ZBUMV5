9d7864aa37129dc9311eb393a8ecc1bbecdb1911
mind0o <75039290+mind0o@users.noreply.github.com>
2021-12-24 20:26:02 +0400

    Update mainCommunicatorV2.js

--- mainCommunicatorV2.js
+++ mainCommunicatorV2.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws')
-const wss = new WebSocket.WebSocketServer({ port: 8080});
+const wss = new WebSocket.WebSocketServer({ port: 3000});
 var express = require('express');
 const cors = require('cors');
 const app = express();
